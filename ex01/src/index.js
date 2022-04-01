41be2bbe2e43908383f5d321d87edd9e9fb2f6e4
zabec <denis.imsirovic@gmail.com>
2022-04-01 14:22:12 +0200

    exercises

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+function main(numOne, numTwo) {
+  // Only change code below this line
+  var myFirstName = "John";
+  var myLastName = "Huston";
+  var myHomeTown = "Sarajevo";
+  // Only change code above this line
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+
+console.log(main());
+
+module.exports = main;
---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+  // Only change code below this line
+  var myString = "First string. " + "Second string.";
+  // Only change code above this line
+  return myString;
+}
+
+console.log(main());
+
+module.exports = main;
